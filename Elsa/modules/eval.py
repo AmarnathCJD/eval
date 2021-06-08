09e0e332146ab831be78dc7a12a5f73df3ba0112
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-08 09:34:55 +0530

    Update eval.py

--- Elsa/modules/eval.py
+++ Elsa/modules/eval.py
@@ -1,1 +1,13 @@
-from Elsa import ubot
+from Elsa.events import Sbot
+
+@Ebot(pattern="^/eval ?(.*)")
+async def ev(event):
+ code = event.text.split(None, 1)[1]
+ if not code:
+    return
+ 
+ 
+@Ebot(pattern="^/ping$")
+async def p(event):
+ await event.edit("Pong!")
+
